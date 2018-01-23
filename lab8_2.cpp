3a89deb1ea133e81cfe4577b9e5e3fb6b2b41aa2
Unknown <mymintzata@hotmail.co.th>
2018-01-23 11:21:26 +0700

    ข้อที่ 8.2 เสร็จ

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,12 +4,28 @@ using namespace std;
 
 int main () {
 
-        char name[] = {"Anukul"};
-        int nameLength = 6;
-        cout << name;
+ //       char city[] = {'A','B','C','\0'};
+ //      char city2[] ={ "ABC" };
+
+        char name[] = {"Worarat"};
+        int nameLength = 7;
+        cout << name<<endl;
 
         // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
         // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
+        char revName[nameLength+1];
+
+        for (int i = 0; i < nameLength; i++) {
+            revName[i] = name[6-i];
+        }
+        revName[nameLength] = '\0';
+        cout << revName << endl;
+
+        for (int i =0; i < 7; i++){
+            revName[i] = revName[i] + 1;
+        }
+
+        cout << revName;
 
 
 
