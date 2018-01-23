61e7d01e9929145d8d3f1f98d713c0de83bd4218
Unknown <mymintzata@hotmail.co.th>
2018-01-23 10:53:47 +0700

    ข้อที่ 8.1 เสร็จ

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -7,6 +7,10 @@ int main () {
         int arr[10] = {3,2,5,5,6,9,1,3,2,7};
 
         // เขียนโปรแกรมเพื่อแสดงค่าใน arr
+        for (int i = 0; i < 10; i++) {
+                cout << arr[i] << endl;
+        }
+
 
         return 0;
 }
